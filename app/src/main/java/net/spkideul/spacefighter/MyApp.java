02608b8512c166a4892418be3200c828c47b76b2
klokov21 <klokovmaksim191@gmail.com>
2019-10-25 10:14:04 +0300

    release

---
+++ app/src/main/java/net/spkideul/spacefighter/MyApp.java
@@ -0,0 +1,17 @@
+package net.spkideul.spacefighter;
+
+import android.app.Application;
+
+import com.onesignal.OneSignal;
+
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+    }
+}
