ab104c931da9de1059fb40995c6d448b5b19700a
klokov21 <klokovmaksim191@gmail.com>
2019-10-28 11:40:01 +0200

    release

--- app/src/main/java/net/spkideul/spacefighter/MyApp.java
+++ app/src/main/java/net/spkideul/spacefighter/MyApp.java
@@ -1,14 +1,27 @@
 package net.spkideul.spacefighter;
 
 import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.onesignal.OneSignal;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MyApp extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        FacebookSdk.fullyInitialize();
+        AppEventsLogger.activateApp(this);
+
         OneSignal.startInit(this)
                 .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
                 .unsubscribeWhenNotificationsAreDisabled(true)
